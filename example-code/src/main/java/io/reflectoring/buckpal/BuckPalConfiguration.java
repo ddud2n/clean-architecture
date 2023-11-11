80218779a256e24f440062134cc91687669206cb
ddud2n <sujin0970@naver.com>
2023-11-11 22:24:39 +0900

    Update BuckPalConfiguration.java

--- example-code/src/main/java/io/reflectoring/buckpal/BuckPalConfiguration.java
+++ example-code/src/main/java/io/reflectoring/buckpal/BuckPalConfiguration.java
@@ -12,7 +12,6 @@ public class BuckPalConfiguration {
 
     /**
      * Adds a use-case-specific {@link MoneyTransferProperties} object to the application context.
-     * 
      * The properties are read from the Spring-Boot-specific {@link BuckPalConfigurationProperties} object.
      */
     @Bean
